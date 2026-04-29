aab2866fcc9517ad62a4b92be84606afc8811ad7
RubenToucedaPRO <ruben.touceda@gmail.com>
2026-04-29 19:49:22 +0200

    Anadir clase registrar

---
+++ escuela/registrar.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+
+def registrar_log(tarea, resultado):
+    fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open("escuela.log", "a", encoding="utf-8") as f:
+        f.write(f"[{fecha}] TAREA: {tarea} | ESTADO: {resultado}\n")
